d2c2419618a2bab365cf5f74452c27d30e3909bf
shruti meshram <shruti meshram>
2022-03-14 21:53:58 +0530

    phase3

---
+++ sportyshooes/src/main/java/sportyshooes/sportyshooes/App.java
@@ -0,0 +1,13 @@
+package sportyshooes.sportyshooes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
